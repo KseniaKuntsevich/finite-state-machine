731e965b27157d8d5518ade0fa719d8ec7a5f004
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2019-10-07 20:36:18 +0300

    feat: add finite state machine task solution

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,71 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.state = config.initial;
+        this.states = config.states;
+        this.history = [this.state];
+        this.currentStage = 0;
+        this.statesList = [];
+        this.eventsStates = {};
+
+        for(let state in this.states) {
+            this.statesList.push(state);
+
+            for(let key in this.states[state]['transitions']) {
+                if(this.eventsStates[key]){ 
+                    this.eventsStates[key].push(state);
+                } else { 
+                    this.eventsStates[key] = [state];
+                }
+            }
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(!this.states[state]) {
+            throw new Error ('Not correct state');
+            return;
+        }
+
+        this.state = state;
+        this.currentStage++;
+        this.history[this.currentStage] = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        let state = this.states[this.state]['transitions'][event];
+
+        if(!state){
+            throw new Error('Not correct event');
+            return;
+        } 
+
+        this.changeState(state);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.history[0])
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +75,51 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if(!arguments.length) return this.statesList;
+
+        if(this.eventsStates[event]) return this.eventsStates[event];
+
+        return [];
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+       let prev = this.history[this.currentStage - 1];
+
+       if(!prev) return false;
+
+       this.state = prev;
+       this.currentStage--;
+       return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        let second = this.history[this.currentStage + 1];
+
+        if(!second) return false;
+
+        this.state = second;
+        this.currentStage++;
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history = [this.state];
+        this.currentStage = 0;
+    }
 }
 
 module.exports = FSM;
